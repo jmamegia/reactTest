eb19e015c7c22de76ca3ce04ec0127a6bd24ac5c
Juanma Megia <jma.megia@gmail.com>
2019-04-17 17:18:14 +0200

    change to hasrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {HashRouter, Route, Switch} from 'react-router-dom'
 
 import Page0 from './components/page0/Page0'
 import Page1 from './components/page1/Paje1'
@@ -8,13 +8,13 @@ import Page2 from './components/page2/Page2'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Page0} />
         <Route exact path="/page1" component={Page1} />
         <Route exact path="/page2" component={Page2} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
