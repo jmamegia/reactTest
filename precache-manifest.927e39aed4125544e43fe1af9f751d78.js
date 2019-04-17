6d0321ed51f258664ec447776270275bf16060a5
Juanma Megia <jma.megia@gmail.com>
2019-04-17 17:18:51 +0200

    Updates

--- precache-manifest.927e39aed4125544e43fe1af9f751d78.js
+++ precache-manifest.927e39aed4125544e43fe1af9f751d78.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "985de032c679f032047f",
+    "revision": "ee0d58941e1739684b97",
     "url": "/reactTest/static/css/main.2608e3f7.chunk.css"
   },
   {
-    "revision": "985de032c679f032047f",
-    "url": "/reactTest/static/js/main.7bc6db0f.chunk.js"
+    "revision": "ee0d58941e1739684b97",
+    "url": "/reactTest/static/js/main.6840561c.chunk.js"
   },
   {
     "revision": "2ce7ab29404c74777852",
     "url": "/reactTest/static/js/runtime~main.3be99dba.js"
   },
   {
-    "revision": "60fb7ff13604b608d6b6",
-    "url": "/reactTest/static/js/2.249b67d5.chunk.js"
+    "revision": "35fceaacfbd903587b9d",
+    "url": "/reactTest/static/js/2.e46e4083.chunk.js"
   },
   {
     "revision": "0831655b8c193fcf216a8ef37912a8a6",
     "url": "/reactTest/static/media/logo.0831655b.svg"
   },
   {
-    "revision": "57711aa51246675f5a3bfbdd40deaa94",
+    "revision": "8a84a4f2282536d75f2b8c22a8d8e3d7",
     "url": "/reactTest/index.html"
   }
 ];
